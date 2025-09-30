3def549d8491d5e8f30935c33c6e00459b28f4cc
Cecília Pinheiro <cissapinheiro09@gmail.com>
2025-09-30 10:29:47 -0300

    fix: erros corrigidos

---
+++ script.js
@@ -0,0 +1,22 @@
+class Jogador {
+    constructor (id, nome, pontos, status = true) {
+        this.id = id;
+        this.nome = nome;
+        this.pontos = pontos;
+        this.status = status;
+    }
+
+    getData(){
+        console.log(`
+            -----Dados do Jogador-----
+            ID: ${this.id}
+            Nome: ${this.nome}
+            Pontos: ${this.pontos}
+            Status: ${this.status ? Ativo : Inativo}
+            ---------------------------`)
+    }
+}
+
+let jogador1 = new Jogador(1, "Zé da manga", 578);
+
+console.table(jogador1);
